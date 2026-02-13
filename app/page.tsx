8881a555d59261274a96034354f4732fc37c037f
tomr1a-ops <tomr1a@gmail.com>
2026-02-13 14:49:05 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,13 @@ export default function Home() {
         justifyContent: "center",
         backgroundColor: "#000",
         color: "#fff",
+        padding: "24px",
       }}
     >
       <h1 style={{ marginBottom: "20px" }}>IMA Studio Player</h1>
 
       <iframe
-     src="https://player.mux.com/EEtT1vz9FZ01DpH4iyByDjwV5w102dhuV0o6EEp12eHMU"
+        src="https://player.mux.com/EEtT1vz9FZ01DpH4iyByDjwV5w102dhuVOo6EEp12eHMU"
         style={{
           width: "90%",
           maxWidth: "900px",
@@ -24,8 +25,7 @@ export default function Home() {
         }}
         allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
         allowFullScreen
-      ></iframe>
+      />
     </div>
   );
 }
-
