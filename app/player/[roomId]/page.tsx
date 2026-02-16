4f1b0877a02303266c9ff7e9724e3f8c833b7c6b
tomr1a-ops <tomr1a@gmail.com>
2026-02-16 17:15:47 -0500

    Update page.tsx

--- app/player/[roomId]/page.tsx
+++ app/player/[roomId]/page.tsx
@@ -1,1 +1,13 @@
+export default function PlayerPage({
+  params,
+}: {
+  params: { roomId: string };
+}) {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>IMAOS Player</h1>
+      <p>Room ID: {params.roomId}</p>
+    </div>
+  );
+}
 
