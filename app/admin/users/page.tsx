238992b77a66ec0f5a1ae7e6d96bcb78bfd63b98
tomr1a-ops <tomr1a@gmail.com>
2026-02-21 14:55:53 -0500

    Update page.tsx

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -1,1 +1,9 @@
+// app/admin/users/page.tsx
+import UsersClient from "./UsersClient";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default function AdminUsersPage() {
+  return <UsersClient />;
+}
