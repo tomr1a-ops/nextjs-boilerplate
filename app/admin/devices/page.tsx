0f71f070a54ea66858a635eb5b5efd33feff6163
tomr1a-ops <tomr1a@gmail.com>
2026-02-22 13:50:44 -0500

    Update page.tsx

--- app/admin/devices/page.tsx
+++ app/admin/devices/page.tsx
@@ -1,1 +1,8 @@
+import DevicesClient from "./DevicesClient";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+export default function AdminDevicesPage() {
+  return <DevicesClient />;
+}
