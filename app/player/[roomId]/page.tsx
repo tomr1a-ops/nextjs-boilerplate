923ebb2ba097dda4f2682864781e508d18aad254
tomr1a-ops <tomr1a@gmail.com>
2026-02-23 13:00:02 -0500

    Update page.tsx

--- app/player/[roomId]/page.tsx
+++ app/player/[roomId]/page.tsx
@@ -1,6 +1,6 @@
 import PlayerClient from "./PlayerClient";
 
-export default async function PlayerPage({ params }: { params: Promise<{ code: string }> }) {
-  const { code } = await params;
-  return <PlayerClient code={code} />;
+export default async function PlayerPage({ params }: { params: Promise<{ roomId: string }> }) {
+  const { roomId } = await params;
+  return <PlayerClient roomId={roomId} />;
 }
