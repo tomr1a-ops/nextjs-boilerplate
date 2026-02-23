b066f55c82183cd52e1463b9818ec8f0cb9595a2
tomr1a-ops <tomr1a@gmail.com>
2026-02-23 12:47:34 -0500

    Delete app/player/page.tsx

--- app/player/page.tsx
+++
@@ -1,6 +0,0 @@
-import PlayerClient from "./PlayerClient";
-
-export default async function PlayerPage({ params }: { params: Promise<{ code: string }> }) {
-  const { code } = await params;
-  return <PlayerClient code={code} />;
-}
